289624a5108e26c7d7a29d9b123b0a31078807b4
fakhrip <fakhriputra123s@gmail.com>
2021-06-06 16:53:08 +0700

    add servo script and refactor for config

--- component-scripts/led.py
+++ component-scripts/led.py
@@ -1,3 +1,5 @@
+from config import pins
+
 import RPi.GPIO as GPIO
 import atexit
 import time
@@ -9,20 +11,14 @@ def blinkLed(duration: int):
 
     time.sleep(duration)
     
-    RED.start(0)
-    GREEN.start(0)
-    BLUE.start(0)
+    RED.stop()
+    GREEN.stop()
+    BLUE.stop()
 
 def cleanup():
     GPIO.cleanup()
 
 GPIO.setmode(GPIO.BCM)
-
-pins = {
-    'r': 17,
-    'g': 27,
-    'b': 22
-}
 
 GPIO.setup(pins['r'], GPIO.OUT)
 GPIO.setup(pins['g'], GPIO.OUT)
@@ -34,7 +30,7 @@ RED = GPIO.PWM(pins['r'], pwm_freq)
 GREEN = GPIO.PWM(pins['g'], pwm_freq)
 BLUE = GPIO.PWM(pins['b'], pwm_freq)
 
-atexit.register(cleanup)
-
 if __name__ == "__main__":
+    atexit.register(cleanup)
+
     blinkLed(5)
---
+++ component-scripts/servo.py
@@ -0,0 +1,35 @@
+from config import pins
+
+import RPi.GPIO as GPIO
+import atexit
+import time
+import sys
+
+def rotateServo(to_rotation: float):
+    """
+    Rotate the servo
+
+    Param:
+        to_rotation = In our hardware, 
+            4 means push the ID Card off
+            12 means let the ID Card in
+    """
+    servo.ChangeDutyCycle(to_rotation)
+    time.sleep(0.5)
+    servo.stop()
+
+def cleanup():
+    GPIO.cleanup()
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(pins['servo'], GPIO.OUT)
+
+pwm_freq = 50
+servo = GPIO.PWM(pins['servo'], pwm_freq)
+servo.start(0)
+
+if __name__ == "__main__":
+    atexit.register(cleanup)
+    
+    rotateServo(int(sys.argv[1]))
