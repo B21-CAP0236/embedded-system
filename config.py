3f84aa3c74e5531558e57606734971ced384fec3
fakhrip <fakhriputra123s@gmail.com>
2021-06-08 14:31:25 +0700

    add new configuration

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Config:
     def __init__(self):
         self.__configuration = {
+            "bansos_id": 1,
             "filename": "captured_card.jpg",
             "nik": [210, 270, 0, 520],
             "nama": [285, 325, 20, 500],
